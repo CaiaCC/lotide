25db23673639a80ae481ee66503293e38e17d0ec
Caia Chuang <caiachung@gmail.com>
2020-07-21 20:04:20 +0000

    modifiy tail.js after discuss with group members

--- tail.js
+++ tail.js
@@ -1,15 +1,42 @@
 const assertEqual = function(array1, array2) {
-	array1.shift();
-	
-  const array1ToString = array1.join("");
-  const array2ToString = array2.join("");
-
-  if (array1ToString === array2ToString) {
-    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
-  } else if (array1ToString !== array2ToString) {
-    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  if (ifEqual(tail(array1), array2)) {
+    console.log(`✅✅✅ Assertion passed: ${tail(array1)} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${tail(array1)} !== ${array2}`);
   }
 };
+
+const tail = function(array) {
+  let newArray = [];
+
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const ifEqual = function(array1, array2) {
+  if (array1.toString() === array2.toString()) {
+    return true;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array1[i] === array2[j]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+};
+
+  
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
 
 assertEqual(["Lighthouse Lab", "Bootcamp"], ["Bootcamp"]);
 assertEqual(["Boot camp", "Bootcamp"], ["Bootcamp"]);

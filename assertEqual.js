fc37a2b352ccfe3380b30f7ffe68b84c8cb4e0d1
Caia Chuang <caiachung@gmail.com>
2020-07-21 02:09:10 +0000

    add assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// var assert = require('assert');
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑🛑🛑Assertion passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("✅✅✅Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Lab", "Bootcamp");
+assertEqual("Boot camp", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(3, 1);
+assertEqual(0.3, 1);
+
+// assert.equal(assertEqual("Lighthouse Lab", "Bootcamp"), "Assertion failed: Lighthouse Lab!==Bootcamp");
+// assert.equal(assertEqual(1, 1), "Assertion passed: 1 === 1")

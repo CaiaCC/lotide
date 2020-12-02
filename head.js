69fc6353ef5a0ac0c6ae90eb7085753e81447141
Caia Chuang <caiachuang@gmail.com>
2020-12-01 17:17:29 -0800

    Refactor.

--- head.js
+++ head.js
@@ -1,15 +1,15 @@
 const head = function (arr) {
   return arr.shift();
 }
-/*
-const assertEqual = function(array, element) {
-	let firstElement = array.shift();
 
-	if (firstElement === element) {
-    console.log(`✅✅✅Assertion passed: ${firstElement} === ${element}`);
-  } else if (firstElement !== element) {
-    console.log(`🛑🛑🛑Assertion failed: ${firstElement} !== ${element}`);
-  }
-};*/
+// const assertEqual = function(array, element) {
+// 	let firstElement = array.shift();
+
+// 	if (firstElement === element) {
+//     console.log(`👍 Assertion passed: ${firstElement} === ${element}`);
+//   } else if (firstElement !== element) {
+//     console.log(`❌ Assertion failed: ${firstElement} !== ${element}`);
+//   }
+// };
 
 module.exports = head;
